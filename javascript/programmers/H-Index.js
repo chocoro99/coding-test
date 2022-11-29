f49542c2c4eeaa893cab518d84266883336857a2
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-11-29 20:58:44 +0900

    프로그래머스 문제:  H-Index 코드 작성

---
+++ javascript/programmers/H-Index.js
@@ -0,0 +1,13 @@
+function solution(citations) {
+  var answer = 0;
+  citations.sort((a, b) => {
+    return b - a;
+  });
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] < i + 1) {
+      answer = i;
+      break;
+    } else answer = i + 1;
+  }
+  return answer;
+}

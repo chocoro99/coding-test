0a430fbbe10ec46dfcee83f27ea45b7224d686cb
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-11-29 20:58:31 +0900

    프로그래머스 문제:  K번째 수 코드 작성

---
+++ javascript/programmers/K번째수.js
@@ -0,0 +1,11 @@
+function solution(array, commands) {
+  var answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    const arr = array.slice(commands[i][0] - 1, commands[i][1]);
+    arr.sort((a, b) => {
+      return a - b;
+    });
+    answer.push(arr[commands[i][2] - 1]);
+  }
+  return answer;
+}
